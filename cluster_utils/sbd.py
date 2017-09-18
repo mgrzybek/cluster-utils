93326a4271b7301f6090b1e546c8e494513dbdd8
Mathieu Grzybek <mathieu.grzybek@gmail.com>
2017-09-18 11:09:42 +0200

    Adds some methods

--- cluster_utils/sbd.py
+++ cluster_utils/sbd.py
@@ -19,4 +19,33 @@ def get_sbd_devices(conf_file):
 
 			if rs:
 				return rs.group(1).split(';')
+def get_sbd_command_line():
+	command_line = [ 'sbd' ]
+
+	conf_file = get_sbd_conf_file()
+	luns = get_sbd_devices(conf_file)
+
+	if len(luns) == 0:
+		print('No device defined in %s' % conf_file)
+		sys.exit(1)
+
+	for lun in luns:
+		command_line.append('-d')
+		command_line.append(lun)
+
+	if os.getuid() != 0:
+		command_line.insert(0, 'sudo')
+
+	return command_line
+
+def sbd_reset_node(node, command_line = None):
+	if command_line == None:
+		command_line = get_sbd_command_line()
+
+	reset_command = command_line
+	reset_command.append('message')
+	reset_command.append(node)
+	reset_command.append('clear')
+
+	return subprocess.check_call(command_line)
 
