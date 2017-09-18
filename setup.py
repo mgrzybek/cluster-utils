4e3f3470dd55b8b2f0d2b271457d9e4dd3f56731
Mathieu Grzybek <mathieu.grzybek@gmail.com>
2017-09-18 11:14:15 +0200

    set version number

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ def read(fname):
 
 setup(
 	name = "cluster-utils",
-	version = "1.1.3",
+	version = "1.0.0",
 	author = "Mathieu Grzybek",
 	author_email = "mathieu@grzybek.fr",
 	description = "Utilitaires pour la gestion des ressources",
 	license = "GPLv3",
 	keywords = "monitoring pacemaker, libvirtd & cgroups",
-	url = "https://git.gendarmerie.fr/stig/cluster-utils",
+	url = "https://github.com/mgrzybek/cluster-utils",
 	packages = ['cluster_utils'],
-	scripts = [ 'bin/cluster_set_cpuset', 'bin/cluster_get_cpuset_allocation', 'bin/cluster_get_mem_allocation', 'bin/cluster_get_cluster_usage', 'bin/sbd_list', 'bin/sbd_dump' ],
+	scripts = [ 'bin/cluster_set_cpuset', 'bin/cluster_get_cpuset_allocation', 'bin/cluster_get_mem_allocation', 'bin/cluster_get_cluster_usage', 'bin/sbd_list', 'bin/sbd_dump', 'sbd_clear_nodes' ],
 	install_requires = [''],
 	long_description = read('README'),
 	classifiers = [
